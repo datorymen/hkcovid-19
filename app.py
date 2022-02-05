b1a3b0f6de0d6789e8810256a1f6bad53e192038
datorymen <87224912+datorymen@users.noreply.github.com>
2022-02-05 18:39:38 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -35,8 +35,8 @@ st.markdown(hide_table_row_index, unsafe_allow_html=True)
 
 # Display a static table
 
-selection = st.selectbox(
-          '請選擇功能', ['個案曾經到訪過的大廈', '各區流動採樣站'])
+selection = st.radio(
+          '請選擇功能：', ['各區流動採樣站', '個案曾經到訪過的大廈' ])
 
 if selection == '個案曾經到訪過的大廈':
 
@@ -47,27 +47,20 @@ if selection == '個案曾經到訪過的大廈':
      df = df[df['個案最後到訪日期'].notnull()]
 
      st.title('個案曾經到訪過的大廈')
-     st.write('數據更新時間：' + now_str)
-     st.write('過去14日範圍 (' + days_14_ago_str + ' 到 ' + yesterday_str + ')')
+     st.write('信息更新時間：' + now_str)
+     st.write('過去14日範圍是指' + days_14_ago_str + ' 到 ' + yesterday_str)
 
      df1 = df.groupby('地區').agg('count').reset_index()
      df1 = df1[['地區', '大廈名單']]
-     df1.columns = ['地區', '14天内的大廈总數量']
-     df1 = df1.sort_values('14天内的大廈总數量', ascending=False).reset_index(drop=True)
+     df1.columns = ['地區', '14天内到訪過的大廈总數量']
+     df1 = df1.sort_values('14天内到訪過的大廈总數量', ascending=False).reset_index(drop=True)
      df1['排名'] = df1.index + 1
-     df1 = df1[['排名', '地區', '14天内的大廈总數量']]
-
-
-
-
-     st.header('18區大廈數量排名：')
-
-     st.table(df1)
+     df1 = df1[['排名', '地區', '14天内到訪過的大廈总數量']]
 
 
      area_list = (df1['地區'].values)
      option = st.selectbox(
-          '選擇地區以查看大廈詳細名稱（按照最新日期排列）', area_list)
+          '選擇地區以查看大廈詳細名稱（按照最新的到訪日期排列）(頁面底部有18區大廈數量排名)', area_list)
 
 
      df2 = df[df['地區'] == option]
@@ -84,10 +77,17 @@ if selection == '個案曾經到訪過的大廈':
 
      st.table(df2)
 
+     st.header('18區大廈數量排名：')
+
+     st.table(df1)
+
+     st.caption('數據來自衛生署。刷新頁面即可更新。')
+
+
 
 if selection == '各區流動採樣站':
      st.title('各區流動採樣站')
-     st.write('數據更新日期：' + today)
+     st.write('信息更新日期：' + today)
      # st.write('正在開發中。。。')
      pdf = pdfplumber.open(today + '-a-mscs.pdf')
      pages = len(pdf.pages)
@@ -115,12 +115,9 @@ if selection == '各區流動採樣站':
 
      # st.write('test')
      st.table(df_pdf)
-
-
+     st.caption('數據來自衛生署。每日更新。')
 
 st.caption('''
-數據來自衛生署。刷新頁面即可更新。\n
-\n
 如果發現問題或者有任何建議，請發郵件到: \n
 number1datascientist(at)gmail點com \n
 \n
